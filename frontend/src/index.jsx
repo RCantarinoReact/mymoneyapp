416e2ae1a831ca12cfc7ca4b9f595f7ce882c3c4
Renato Cantarino <renato.cantarino@gmail.com>
2019-08-24 14:23:19 -0300

    templates

---
+++ frontend/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './main/app'
+
+const elemento = document.getElementById('app')
+ReactDOM.render(
+    <div>
+        <App />
+    </div>, elemento)
+
---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,13 @@
+import '../commons/template/dependecies'
+import React from 'react'
+
+//
+import Header from '../commons/template/header'
+
+
+
+export default props => (
+    <div className='wrapper'>
+        <Header/>
+    </div>
+)
